0c343e9094479d2b37b99eb21b8a3f63d4efdc01
Alisina Sarwari <150026622+Alisina137@users.noreply.github.com>
2026-09-22 18:53:53 +0430

    Move history header slightly lower

--- app/(tabs)/history.tsx
+++ app/(tabs)/history.tsx
@@ -157,7 +157,7 @@ const styles = StyleSheet.create({
     flex: 1,
     paddingHorizontal: 18,
     paddingBottom: 18,
-    paddingTop: 30
+    paddingTop: 38
   },
   header: {
     flexDirection: "row-reverse",
